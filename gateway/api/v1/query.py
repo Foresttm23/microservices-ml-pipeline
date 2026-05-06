16b4c58abf32ad6c74a7fa8d5c8d0b38280ae64b
Foresttm23 <max.gogulia@gmail.com>
2026-05-06 18:19:42 +0200

    feat(gateway/query): update message request.

--- gateway/api/v1/query.py
+++ gateway/api/v1/query.py
@@ -2,16 +2,15 @@ from fastapi import APIRouter, Request
 
 from gateway.core.config import get_settings
 from gateway.core.dependencies import HTTPXClientDep
-from gateway.schemas.query import BaseQuery
+from shared.schemas import PipelineRequest
 from shared.utils import forward_to_service
 
 router = APIRouter()
 
 
-# Todo add middleware to verify that the message is not null before passing a post request.
 @router.post("/pipelines/{pipeline_id}/run")
 async def proxy_to_orchestrator(
-    message: BaseQuery, pipeline_id: str, request: Request, client: HTTPXClientDep
+    payload: PipelineRequest, pipeline_id: str, request: Request, client: HTTPXClientDep
 ):
     gateway_settings = get_settings()
     orchestrator_url = f"{gateway_settings.ORCHESTRATOR_URL}/api/run/{pipeline_id}"
